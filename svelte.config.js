f200e5849de8d17e4132b98a2b6f052821f46cae
Martin <acostajoma9@gmail..com>
2023-04-25 18:29:14 -0600

    Changing the svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      routes: {
+          include: ['/*'],
+      }
+  })
   },
   preprocess: vitePreprocess(),
 };
